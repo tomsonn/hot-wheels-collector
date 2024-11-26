493d6e7e4becb6db82ac75cdc96fe6c9e53211a4
Tomáš Macko <tomik.macko@gmail.com>
2024-11-26 22:57:52 +0100

    feat: get_model endpoint

--- hot_wheels_collector/api.py
+++ hot_wheels_collector/api.py
@@ -2,9 +2,12 @@ from collections.abc import AsyncIterator
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from starlette.responses import JSONResponse
 from structlog.stdlib import BoundLogger
 
 from hot_wheels_collector.database.engine import Database
+from hot_wheels_collector.database.repository import HotWheelsRepository, HotWheelsRepositoryDependency
+from hot_wheels_collector.models.api import Model
 from hot_wheels_collector.settings.base import Settings
 from hot_wheels_collector.settings.logger import configure_logger
 
@@ -14,9 +17,11 @@ async def lifespan(app: FastAPI) -> AsyncIterator[None]:
     settings = Settings()
     logger = configure_logger()
     db = Database(settings)
+    hw_repository = HotWheelsRepository(db, logger)
 
     app.dependency_overrides[Settings] = lambda: settings
     app.dependency_overrides[Database] = lambda: db
+    app.dependency_overrides[HotWheelsRepository] = lambda: hw_repository
     app.dependency_overrides[BoundLogger] = lambda: logger
 
     yield
@@ -34,3 +39,12 @@ app = FastAPI(
 @app.get("/ping")
 def ping() -> dict:
     return {"ping": "pong"}
+
+
+@app.get("/model/{model_id}", response_model=Model, tags=["Hot Wheels"])
+async def get_model(model_id: int, hw_repository: HotWheelsRepositoryDependency) -> Model | JSONResponse:
+    model = await hw_repository.get_model(model_id)
+    if not model:
+        return JSONResponse({"error": "not_found"}, 404)
+
+    return model
---
+++ hot_wheels_collector/database/repository.py
@@ -0,0 +1,21 @@
+from typing import Annotated
+
+from fastapi import Depends
+from sqlmodel import select
+
+from hot_wheels_collector.database.engine import DatabaseDependency
+from hot_wheels_collector.database.models import Models
+from hot_wheels_collector.settings.logger import LoggerDependency
+
+
+class HotWheelsRepository:
+    def __init__(self, db: DatabaseDependency, logger: LoggerDependency) -> None:
+        self.__db = db
+        self.__logger = logger
+
+    async def get_model(self, model_id: int) -> Models | None:
+        async with self.__db.acquire() as session:
+            return (await session.execute(select(Models).where(Models.id == model_id))).first()
+
+
+HotWheelsRepositoryDependency = Annotated[HotWheelsRepository, Depends(HotWheelsRepository)]
